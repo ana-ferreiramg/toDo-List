c5ae1969be525d77e6e339ad88f44030cad17d7f
Ana Ferreira <anapaulaferreira0309@gmail.com>
2021-06-13 17:05:30 -0300

    update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,41 +1,45 @@
 const listTodos = document.querySelector('#wrapper #todos ul');
 const task = document.querySelector('#wrapper input');
 const add = document.querySelector('#wrapper .group-add-tasks-button button');
-const inputAddTodo = document.getElementById("task");
+const inputAddTodo = document.getElementById('task');
 
 let todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
-const ID = () => '_' + Math.random().toString(36).substr(2, 9);
+const Utils = {
+  ID() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  },
+};
 
 function addTodo(item) {
-    if (item !== '' && item != ' ') {
-        const todo = {
-            id: ID(),
-            task: item,
-            completed: false
-        };
+  if (item !== '' && item != ' ') {
+    const todo = {
+      id: Utils.ID(),
+      task: item,
+      completed: false,
+    };
 
-        todos.push(todo);
-        saveToStorage(todos);
+    todos.push(todo);
+    saveToStorage(todos);
 
-        task.value = '';
-    }
+    task.value = '';
+  }
 }
 add.addEventListener('click', () => addTodo(task.value));
-inputAddTodo.addEventListener('keyup', (e) => e.keyCode === 13 ? addTodo(task.value) : console.log("Not ENTER key"));
+inputAddTodo.addEventListener('keyup', (e) => (e.keyCode === 13 ? addTodo(task.value) : 0));
 
 function show(todos) {
-    listTodos.innerHTML = '';
+  listTodos.innerHTML = '';
 
-    todos.forEach((item) => {
-        const checked = item.completed ? 'checked': null;
-        const li = document.createElement('li');
+  todos.forEach((item) => {
+    const checked = item.completed ? 'checked' : null;
+    const li = document.createElement('li');
 
-        if (item.completed === true) {
-            li.classList.add('checked');
-        }
+    if (item.completed === true) {
+      li.classList.add('checked');
+    }
 
-        li.innerHTML = `
+    li.innerHTML = `
             <div data-key=${item.id}>
                 <input type="checkbox" class="checkbox" ${checked}>
                 ${item.task}
@@ -46,44 +50,38 @@ function show(todos) {
             </div>
         `;
 
-        listTodos.append(li);
-    });
+    listTodos.append(li);
+  });
 }
 
 function saveToStorage(todos) {
-    localStorage.setItem('list_todos', JSON.stringify(todos));
-    show(todos);
+  localStorage.setItem('list_todos', JSON.stringify(todos));
+  show(todos);
 }
 
 function getFromLocalStorage() {
-    const reference = localStorage.getItem('list_todos');
+  const reference = localStorage.getItem('list_todos');
 
-    if (reference) {
-        todos = JSON.parse(reference);
-        show(todos);
-    }
+  if (reference) {
+    todos = JSON.parse(reference);
+    show(todos);
+  }
 }
 
 function toogle(id) {
-    todos.forEach(item => (item.id == id) ? item.completed = !item.completed : item.completed);
+  todos.forEach((item) => (item.id == id ? (item.completed = !item.completed) : item.completed));
 
-    saveToStorage(todos);
+  saveToStorage(todos);
 }
 
 function deleteTodo(id) {
-    todos = todos.filter(item => item.id != id);
+  todos = todos.filter((item) => item.id != id);
 
-    saveToStorage(todos);
+  saveToStorage(todos);
 }
-
 getFromLocalStorage();
 
-listTodos.addEventListener("click", (e) => {
-    if (e.target.type === 'checkbox') {
-        toogle(e.target.parentElement.getAttribute('data-key'));
-    }
-
-    if (e.target.classList.contains('delete-button')) {
-        deleteTodo(e.target.parentElement.getAttribute('data-key'));
-    }
+listTodos.addEventListener('click', (e) => {
+  if (e.target.type === 'checkbox') toogle(e.target.parentElement.getAttribute('data-key'));
+  if (e.target.classList.contains('delete-button')) deleteTodo(e.target.parentElement.getAttribute('data-key'));
 });
